c80df5f26c6cee98fb6ded2a259e128af797c5b2
john-mirage <28307090+john-mirage@users.noreply.github.com>
2022-12-04 06:36:55 +0100

    fix: fix html file and main css/js files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import "./main.css";
 import WebApp from "@components/web-app";
+import WebStep from "@components/web-step";
 
 customElements.define("web-app", WebApp);
+customElements.define("web-step", WebStep, { extends: "li" });
 
 const app = document.getElementById("app");
 const webApp = document.createElement("web-app");

2cdf0b4d6eb0b1423a5fc3001772d0c5eefb23e9
john-mirage <28307090+john-mirage@users.noreply.github.com>
2022-12-04 04:40:42 +0100

    fix:(main): append web app manually

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import "./main.css";
 import WebApp from "@components/web-app";
 
-customElements.define("web-app", WebApp, { extends: "main" });
+customElements.define("web-app", WebApp);
+
+const app = document.getElementById("app");
+const webApp = document.createElement("web-app");
+app?.replaceChildren(webApp);

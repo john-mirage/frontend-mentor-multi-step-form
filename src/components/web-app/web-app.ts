bd32c916db0c986256aeabfe2c8cdbc3699d1c11
john-mirage <28307090+john-mirage@users.noreply.github.com>
2022-12-04 06:35:17 +0100

    feat(component): add web app content and style

--- src/components/web-app/web-app.ts
+++ src/components/web-app/web-app.ts
@@ -1,12 +1,19 @@
 class WebApp extends HTMLElement {
   #hasBeenMountedOnce = false;
+  readonly #headerElement;
+  readonly #bodyElement;
 
   constructor() {
     super();
+    const template = <HTMLTemplateElement>document.getElementById("template-web-app");
+    this.#headerElement = (<HTMLElement>template.content.firstElementChild).cloneNode(true);
+    this.#bodyElement = (<HTMLDivElement>template.content.lastElementChild).cloneNode(true);
   }
 
   connectedCallback() {
     if (!this.#hasBeenMountedOnce) {
+      this.classList.add("web-app");
+      this.replaceChildren(this.#headerElement, this.#bodyElement);
       this.#hasBeenMountedOnce = true;
     }
   }
